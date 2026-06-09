24d9aea4505e1a7506c5eaf966f8a8e9edd9f6c4
Ivan Spasov <ivanspasov157@gmail.com>
2026-06-09 14:18:56 +0300

    Installed and cofigured Tailwind

---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar(){
+    return(
+        <aside className="border-2 border-red-500 background-color: var(--color-white) size-16">
+        <nav>
+        sidebar
+        something 
+
+        </nav>
+        </aside>
+    );
+}
+
+export default Navbar;
